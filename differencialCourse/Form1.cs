88ecf79f8be2b3e4ce63fe3e76653a3312870865
CkaDyDeJI <55078827+CkaDyDeJI@users.noreply.github.com>
2020-05-18 23:45:12 +0300

    comments without adams

--- differencialCourse/Form1.cs
+++ differencialCourse/Form1.cs
@@ -1,51 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace differencialCourse
 {
-    using static System.Convert;
+    using static Convert;
+
 
     public partial class Form1 : Form
     {
-        double[] startValues_ = new double[4];
+        private readonly double[] startValues_ = new double[4]; //входные значения будут хваниться ту
 
 
-        public Form1 ()
+        public Form1()
         {
-            InitializeComponent ();
+            InitializeComponent();
         }
 
 
-        private void calculateButton_Click (object sender, EventArgs e)
+        private void calculateButton_Click (object sender, EventArgs e) //нажатие на кнопку рассчета штука
         {
-            if (initializeField() == false) {
-                MessageBox.Show ("input correct values");
+            if (initializeField() == false) {   //функция проверки входных параметров
+                MessageBox.Show ("input correct values");   //сообщения о неправильном вводе
 
                 return;
             }
 
 
+            var dropDownMethod = methodComboBox.SelectedIndex;  //сохранение индекса выбранного способо решения
 
-            int dropDownMethod = methodComboBox.SelectedIndex;
+            if (dropDownMethod != -1) calculatedData.Rows.Clear();  //очистка таблицы, чтобы она пересоздавалась
 
-            if (dropDownMethod != -1) {
-                calculatedData.Rows.Clear();
-            }
-
-            if (dropDownMethod == 0)
-                eulerMethod();
-            else {
+            if (dropDownMethod == 0) {
+                eulerMethod();  //если выбран эйлер, то эйлер
+            } else {
                 if (dropDownMethod == 1)
-                    rungeKutteMethod();
-                else 
-                    adamsMethod();
+                    rungeKutteMethod(); //если рунге кутты, то рунге кутты
+                else
+                    adamsMethod();  //метод адамса
             }
         }
 
@@ -53,34 +46,34 @@ namespace differencialCourse
         private bool initializeField()
         {
             try {
-                startValues_[0] = ToDouble ((leftBorderBox.Text).Replace ('.', ','));
+                startValues_[0] = ToDouble (leftBorderBox.Text.Replace ('.', ',')); //левая граница и по совместительству х0
             }
             catch {
-                MessageBox.Show ("incorrect left border");
+                MessageBox.Show ("incorrect left border");  //иначе сообщить пользователю и возврат 
 
                 return false;
             }
 
             try {
-                startValues_[1] = ToDouble ((rightBorderBox.Text).Replace ('.', ','));
+                startValues_[1] = ToDouble (rightBorderBox.Text.Replace ('.', ','));    //правая граница интервала
             }
             catch {
-                MessageBox.Show ( "incorrect right border" );
+                MessageBox.Show ("incorrect right border");
 
                 return false;
             }
 
             try {
-                startValues_[2] = ToDouble ((stepBox.Text).Replace ('.', ','));
+                startValues_[2] = ToDouble (stepBox.Text.Replace ('.', ','));   //шаг
             }
             catch {
-                MessageBox.Show ( "incorrect step" );
+                MessageBox.Show ("incorrect step");
 
                 return false;
             }
 
             try {
-                startValues_[3] = ToDouble ((yTextBox.Text).Replace ('.', ','));
+                startValues_[3] = ToDouble (yTextBox.Text.Replace ('.', ','));  //у0
             }
             catch {
                 MessageBox.Show ("incorrect y0");
@@ -88,79 +81,76 @@ namespace differencialCourse
                 return false;
             }
 
-            return true;
+            return true;    //если все нормально считалось, то выполнение программы продолжается
         }
 
 
-        private void eulerMethod()
+        private void eulerMethod()  //эйлер
         {
-            double step = startValues_[2];
+            var step = startValues_[2]; //значения шага внутри функции
 
-            calculatedData.Rows.Add ();
-            calculatedData.Rows[0].Cells[0].Value = startValues_.First();
-            calculatedData.Rows[0].Cells[1].Value = startValues_.Last();
+            calculatedData.Rows.Add();  //добавление строки в таблицу
+            calculatedData.Rows[0].Cells[0].Value = startValues_.First();   //внос в таблицу х0
+            calculatedData.Rows[0].Cells[1].Value = startValues_.Last();    //у0
 
 
-            int k = 0;
-            double previousY = 0;
-            double previousYd = 0;
-            for (double i = startValues_[0]; i < startValues_[1]; i += step)
-            {
-                calculatedData.Rows.Add ();
-                previousY = ToDouble ( calculatedData.Rows[k].Cells[1].Value );
-                previousYd = step * F ( i, previousY );
+            var k = 0;  //номер строки, с которой работаем
+            double previousY = 0;   //предыдущий вычисленный y0
+            double previousYd = 0;  //предыдущая вычисленная производная от у
+            for (var i = startValues_[0]; i < startValues_[1]; i += step) { //проход по всему интервалу с шагом
+                calculatedData.Rows.Add();
+                previousY = ToDouble (calculatedData.Rows[k].Cells[1].Value);   //достаем из таблицы последний вычисленный y
+                previousYd = step * F (i, previousY);   //вычисляем новую производную от y
 
-                calculatedData.Rows[k].Cells[2].Value = previousYd;
-                k++;
+                calculatedData.Rows[k].Cells[2].Value = previousYd; //записываем в таблицу
+                k++;    //переходим на следующую строку
 
-                calculatedData.Rows[k].Cells[0].Value = i + step;
-                calculatedData.Rows[k].Cells[1].Value = previousYd + previousY;
+                calculatedData.Rows[k].Cells[0].Value = i + step;   //записываем в таблицу новый х (xi + 1)
+                calculatedData.Rows[k].Cells[1].Value = previousYd + previousY; //новый y (yi)
             }
         }
 
 
-        private void rungeKutteMethod()
+        private void rungeKutteMethod() //рунге кутты (тут все то же самое, но с промежуточным вычисление коэффициентов)
         {
-            double step = startValues_[2];
+            var step = startValues_[2];
 
-            calculatedData.Rows.Add ();
-            calculatedData.Rows[0].Cells[0].Value = startValues_.First ();
-            calculatedData.Rows[0].Cells[1].Value = startValues_.Last ();
+            calculatedData.Rows.Add();
+            calculatedData.Rows[0].Cells[0].Value = startValues_.First();
+            calculatedData.Rows[0].Cells[1].Value = startValues_.Last();
 
-            int k = 0;
-            double k1 = 0;
+            var k = 0;
+            double k1 = 0;  //переменные для коэффициентов
             double k2 = 0;
             double k3 = 0;
             double k4 = 0;
             double previousY = 0;
             double previousYd = 0;
-            for (double i = startValues_[0]; i < startValues_[1]; i += step)
-            {
-                calculatedData.Rows.Add ();
-                previousY = ToDouble ( calculatedData.Rows[k].Cells[1].Value );
-                k1 = step * F (i, previousY);
+            for (var i = startValues_[0]; i < startValues_[1]; i += step) {
+                calculatedData.Rows.Add();
+                previousY = ToDouble (calculatedData.Rows[k].Cells[1].Value);   //запись предыдущего у в переменную
+                k1 = step * F (i, previousY);   //вычисление к1
                 k2 = step * F (i + step / 2.0, previousY + k1 / 2.0);
                 k3 = step * F (i + step / 2.0, previousY + k2 / 2.0);
                 k4 = step * F (i + step, previousY + k3);
 
-                previousYd = 1.0 / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4);
+                previousYd = 1.0 / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4);   //вычисление производной от у
 
-                calculatedData.Rows[k].Cells[2].Value = previousYd;
+                calculatedData.Rows[k].Cells[2].Value = previousYd; //добавление ее в таблицу
                 k++;
 
-                calculatedData.Rows[k].Cells[0].Value = i + step;
-                calculatedData.Rows[k].Cells[1].Value = previousYd + previousY;
+                calculatedData.Rows[k].Cells[0].Value = i + step;   //добавление след х
+                calculatedData.Rows[k].Cells[1].Value = previousYd + previousY; //след у
             }
         }
 
 
-        private void adamsMethod()
+        private void adamsMethod()  //адамса
         {
-
         }
 
 
-        private double F (double x, double y)
+        private double F (double x, double y)   //заданная функция
         {
             return x + y * Math.Cos (x);
             //return x * x - 2 * y;
