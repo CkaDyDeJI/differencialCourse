a60657f2deb03f78b2006dfb5deca420b7cd86d9
CkaDyDeJI <55078827+CkaDyDeJI@users.noreply.github.com>
2020-05-20 00:19:07 +0300

    final?

--- differencialCourse/Form1.Designer.cs
+++ differencialCourse/Form1.Designer.cs
@@ -11,13 +11,13 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose (bool disposing)
+        protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
-                components.Dispose ();
+                components.Dispose();
             }
-            base.Dispose ( disposing );
+            base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
@@ -26,7 +26,7 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent ()
+        private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
@@ -36,6 +36,9 @@
             this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.y = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Yy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.yLabel = new System.Windows.Forms.ToolStripLabel();
+            this.yTextBox = new System.Windows.Forms.ToolStripTextBox();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
             this.leftBorderBox = new System.Windows.Forms.ToolStripTextBox();
@@ -48,10 +51,14 @@
             this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
             this.methodComboBox = new System.Windows.Forms.ToolStripComboBox();
             this.calculateButton = new System.Windows.Forms.ToolStripButton();
-            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
-            this.yLabel = new System.Windows.Forms.ToolStripLabel();
-            this.yTextBox = new System.Windows.Forms.ToolStripTextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rungeData = new System.Windows.Forms.DataGridView();
+            this.Xk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Yk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Yks = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.toolStripContainer1.ContentPanel.SuspendLayout();
             this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
             this.toolStripContainer1.SuspendLayout();
@@ -60,18 +67,20 @@
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.calculatedData)).BeginInit();
-            this.toolStrip1.SuspendLayout();
             this.toolStrip2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rungeData)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 30);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.Size = new System.Drawing.Size(237, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Исходная функция";
+            this.label1.Text = "y\' = 5xy + 2y - x";
             // 
             // toolStripContainer1
             // 
@@ -101,12 +110,12 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
             this.splitContainer1.Panel1.Controls.Add(this.label1);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.calculatedData);
+            this.splitContainer1.Panel2.Controls.Add(this.rungeData);
             this.splitContainer1.Size = new System.Drawing.Size(979, 558);
             this.splitContainer1.SplitterDistance = 142;
             this.splitContainer1.TabIndex = 2;
@@ -143,6 +152,29 @@
             this.Yy.HeaderText = "y\'";
             this.Yy.Name = "Yy";
             this.Yy.ReadOnly = true;
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yLabel,
+            this.yTextBox});
+            this.toolStrip2.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(133, 25);
+            this.toolStrip2.TabIndex = 1;
+            // 
+            // yLabel
+            // 
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(19, 22);
+            this.yLabel.Text = "y0";
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(100, 25);
             // 
             // toolStrip1
             // 
@@ -225,6 +257,7 @@
             "Adams"});
             this.methodComboBox.Name = "methodComboBox";
             this.methodComboBox.Size = new System.Drawing.Size(121, 25);
+            this.methodComboBox.SelectedIndexChanged += new System.EventHandler(this.methodComboBox_SelectedIndexChanged);
             // 
             // calculateButton
             // 
@@ -236,28 +269,60 @@
             this.calculateButton.Text = "calculate";
             this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
             // 
-            // toolStrip2
+            // rungeData
             // 
-            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.None;
-            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.yLabel,
-            this.yTextBox});
-            this.toolStrip2.Location = new System.Drawing.Point(3, 0);
-            this.toolStrip2.Name = "toolStrip2";
-            this.toolStrip2.Size = new System.Drawing.Size(133, 25);
-            this.toolStrip2.TabIndex = 1;
+            this.rungeData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.rungeData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.rungeData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Xk,
+            this.Yk,
+            this.k1,
+            this.k2,
+            this.k3,
+            this.k4,
+            this.Yks});
+            this.rungeData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rungeData.Location = new System.Drawing.Point(0, 0);
+            this.rungeData.Name = "rungeData";
+            this.rungeData.RowHeadersVisible = false;
+            this.rungeData.Size = new System.Drawing.Size(979, 412);
+            this.rungeData.TabIndex = 1;
+            this.rungeData.Visible = false;
             // 
-            // yLabel
+            // Xk
             // 
-            this.yLabel.Name = "yLabel";
-            this.yLabel.Size = new System.Drawing.Size(19, 22);
-            this.yLabel.Text = "y0";
+            this.Xk.HeaderText = "X";
+            this.Xk.Name = "Xk";
             // 
-            // yTextBox
+            // Yk
             // 
-            this.yTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.yTextBox.Name = "yTextBox";
-            this.yTextBox.Size = new System.Drawing.Size(100, 25);
+            this.Yk.HeaderText = "Y";
+            this.Yk.Name = "Yk";
+            // 
+            // k1
+            // 
+            this.k1.HeaderText = "k1";
+            this.k1.Name = "k1";
+            // 
+            // k2
+            // 
+            this.k2.HeaderText = "k2";
+            this.k2.Name = "k2";
+            // 
+            // k3
+            // 
+            this.k3.HeaderText = "k3";
+            this.k3.Name = "k3";
+            // 
+            // k4
+            // 
+            this.k4.HeaderText = "k4";
+            this.k4.Name = "k4";
+            // 
+            // Yks
+            // 
+            this.Yks.HeaderText = "Y\'";
+            this.Yks.Name = "Yks";
             // 
             // richTextBox1
             // 
@@ -289,10 +354,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.calculatedData)).EndInit();
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
             this.toolStrip2.ResumeLayout(false);
             this.toolStrip2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rungeData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -321,7 +387,14 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn x;
         private System.Windows.Forms.DataGridViewTextBoxColumn y;
         private System.Windows.Forms.DataGridViewTextBoxColumn Yy;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView rungeData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Xk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Yk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Yks;
     }
 }
 
--- differencialCourse/Form1.cs
+++ differencialCourse/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -10,7 +11,7 @@ namespace differencialCourse
 
     public partial class Form1 : Form
     {
-        private readonly double[] startValues_ = new double[4]; //входные значения будут хваниться ту
+        private readonly double[] startValues_ = new double[4]; //входные значения будут хваниться тут
 
 
         public Form1()
@@ -19,64 +20,70 @@ namespace differencialCourse
         }
 
 
-        private void calculateButton_Click (object sender, EventArgs e) //нажатие на кнопку рассчета штука
+        private void calculateButton_Click(object sender, EventArgs e) //нажатие на кнопку рассчета штука
         {
-            if (initializeField() == false) {   //функция проверки входных параметров
-                MessageBox.Show ("input correct values");   //сообщения о неправильном вводе
+            if (initializeField() == false)
+            {   //функция проверки входных параметров
+                MessageBox.Show("input correct values");   //сообщения о неправильном вводе
 
                 return;
             }
 
+            var selectedMethod = methodComboBox.SelectedIndex;  //сохранение индекса выбранного способо решения
 
             var dropDownMethod = methodComboBox.SelectedIndex;  //сохранение индекса выбранного способо решения
 
             if (dropDownMethod != -1) calculatedData.Rows.Clear();  //очистка таблицы, чтобы она пересоздавалась
 
-            if (dropDownMethod == 0) {
+            if (dropDownMethod == 0)
                 eulerMethod();  //если выбран эйлер, то эйлер
-            } else {
-                if (dropDownMethod == 1)
-                    rungeKutteMethod(); //если рунге кутты, то рунге кутты
-                else
-                    adamsMethod();  //метод адамса
-            }
+            else
+                rungeKutteMethod();
         }
 
 
         private bool initializeField()
         {
-            try {
-                startValues_[0] = ToDouble (leftBorderBox.Text.Replace ('.', ',')); //левая граница и по совместительству х0
+            try
+            {
+                startValues_[0] = ToDouble(leftBorderBox.Text.Replace('.', ',')); //левая граница и по совместительству х0
             }
-            catch {
-                MessageBox.Show ("incorrect left border");  //иначе сообщить пользователю и возврат 
+            catch
+            {
+                MessageBox.Show("incorrect left border");  //иначе сообщить пользователю и возврат 
 
                 return false;
             }
 
-            try {
-                startValues_[1] = ToDouble (rightBorderBox.Text.Replace ('.', ','));    //правая граница интервала
+            try
+            {
+                startValues_[1] = ToDouble(rightBorderBox.Text.Replace('.', ','));    //правая граница интервала
             }
-            catch {
-                MessageBox.Show ("incorrect right border");
+            catch
+            {
+                MessageBox.Show("incorrect right border");
 
                 return false;
             }
 
-            try {
-                startValues_[2] = ToDouble (stepBox.Text.Replace ('.', ','));   //шаг
+            try
+            {
+                startValues_[2] = ToDouble(stepBox.Text.Replace('.', ','));   //шаг
             }
-            catch {
-                MessageBox.Show ("incorrect step");
+            catch
+            {
+                MessageBox.Show("incorrect step");
 
                 return false;
             }
 
-            try {
-                startValues_[3] = ToDouble (yTextBox.Text.Replace ('.', ','));  //у0
+            try
+            {
+                startValues_[3] = ToDouble(yTextBox.Text.Replace('.', ','));  //у0
             }
-            catch {
-                MessageBox.Show ("incorrect y0");
+            catch
+            {
+                MessageBox.Show("incorrect y0");
 
                 return false;
             }
@@ -85,8 +92,28 @@ namespace differencialCourse
         }
 
 
+        private void methodComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (methodComboBox.SelectedIndex == 0)
+            {
+                calculatedData.Visible = true;
+                rungeData.Visible = false;
+            }
+            else
+            {
+                if (methodComboBox.SelectedIndex == 1)
+                {
+                    calculatedData.Visible = false;
+                    rungeData.Visible = true;
+                }
+            }
+        }
+
+
         private void eulerMethod()  //эйлер
         {
+            calculatedData.Rows.Clear();    //таблица для эйлера
+
             var step = startValues_[2]; //значения шага внутри функции
 
             calculatedData.Rows.Add();  //добавление строки в таблицу
@@ -97,10 +124,11 @@ namespace differencialCourse
             var k = 0;  //номер строки, с которой работаем
             double previousY = 0;   //предыдущий вычисленный y0
             double previousYd = 0;  //предыдущая вычисленная производная от у
-            for (var i = startValues_[0]; i < startValues_[1]; i += step) { //проход по всему интервалу с шагом
+            for (var i = startValues_[0]; i < startValues_[1]; i += step)
+            { //проход по всему интервалу с шагом
                 calculatedData.Rows.Add();
-                previousY = ToDouble (calculatedData.Rows[k].Cells[1].Value);   //достаем из таблицы последний вычисленный y
-                previousYd = step * F (i, previousY);   //вычисляем новую производную от y
+                previousY = ToDouble(calculatedData.Rows[k].Cells[1].Value);   //достаем из таблицы последний вычисленный y
+                previousYd = step * F(i, previousY);   //вычисляем новую производную от y
 
                 calculatedData.Rows[k].Cells[2].Value = previousYd; //записываем в таблицу
                 k++;    //переходим на следующую строку
@@ -113,11 +141,13 @@ namespace differencialCourse
 
         private void rungeKutteMethod() //рунге кутты (тут все то же самое, но с промежуточным вычисление коэффициентов)
         {
+            rungeData.Rows.Clear(); //таблица для рунге кутты
+
             var step = startValues_[2];
 
-            calculatedData.Rows.Add();
-            calculatedData.Rows[0].Cells[0].Value = startValues_.First();
-            calculatedData.Rows[0].Cells[1].Value = startValues_.Last();
+            rungeData.Rows.Add();
+            rungeData.Rows[0].Cells[0].Value = startValues_.First();
+            rungeData.Rows[0].Cells[1].Value = startValues_.Last();
 
             var k = 0;
             double k1 = 0;  //переменные для коэффициентов
@@ -126,36 +156,34 @@ namespace differencialCourse
             double k4 = 0;
             double previousY = 0;
             double previousYd = 0;
-            for (var i = startValues_[0]; i < startValues_[1]; i += step) {
-                calculatedData.Rows.Add();
-                previousY = ToDouble (calculatedData.Rows[k].Cells[1].Value);   //запись предыдущего у в переменную
-                k1 = step * F (i, previousY);   //вычисление к1
-                k2 = step * F (i + step / 2.0, previousY + k1 / 2.0);
-                k3 = step * F (i + step / 2.0, previousY + k2 / 2.0);
-                k4 = step * F (i + step, previousY + k3);
+            for (var i = startValues_[0]; i < startValues_[1]; i += step)
+            {
+                rungeData.Rows.Add();
+                previousY = ToDouble(rungeData.Rows[k].Cells[1].Value);   //запись предыдущего у в переменную
 
-                previousYd = 1.0 / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4);   //вычисление производной от у
+                k1 = step * F(i, previousY);   //вычисление к1
+                k2 = step * F(i + step / 2.0, previousY + k1 / 2.0);
+                k3 = step * F(i + step / 2.0, previousY + k2 / 2.0);
+                k4 = step * F(i + step, previousY + k3);
+                rungeData.Rows[k].Cells[2].Value = k1;  //внесение коэффов в таблицу
+                rungeData.Rows[k].Cells[3].Value = k2;
+                rungeData.Rows[k].Cells[4].Value = k3;
+                rungeData.Rows[k].Cells[5].Value = k4;
 
-                calculatedData.Rows[k].Cells[2].Value = previousYd; //добавление ее в таблицу
+                previousYd = 1.0 / 6.0 * (k1 + (2 * k2) + (2 * k3) + k4);   //вычисление производной от у
+
+                rungeData.Rows[k].Cells[6].Value = previousYd; //добавление ее в таблицу
                 k++;
 
-                calculatedData.Rows[k].Cells[0].Value = i + step;   //добавление след х
-                calculatedData.Rows[k].Cells[1].Value = previousYd + previousY; //след у
+                rungeData.Rows[k].Cells[0].Value = i + step;   //добавление след х
+                rungeData.Rows[k].Cells[1].Value = previousYd + previousY; //след у
             }
-        }
-
-
-        private void adamsMethod()  //адамса
-        {
         }
 
 
         private double F (double x, double y)   //заданная функция
         {
             return x + y * Math.Cos (x);
-            //return x * x - 2 * y;
-            //return 0.1 * x * y;
-            //return (5 * x * y) + (2 * y) - x;
         }
     }
 }
